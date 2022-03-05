ff536f6351ee7eee876e8cd9dac45e99278fd217
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-03-05 12:24:08 +0700

    Create a new repository including the resources

---
+++ app/src/main/java/me/daffakurnia/android/firstsubmission/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.daffakurnia.android.firstsubmission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
